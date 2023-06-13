11461f28e600dc0aac6061cc65e4eeb408b0b3c1
Jan Chochole <jan@chochole.com>
2023-06-13 08:58:13 +0000

    done

--- Eclipse/src/com/ArrayDemo.java
+++ Eclipse/src/com/ArrayDemo.java
@@ -1,11 +1,10 @@
 package com;
 
-public class Demo {
+public class ArrayDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
-		System.out.printf("Hello" + "Hello");
-		}
+
+	}
 
 }
